881b564deb35ae85d235bc1cde4ce60be2fc4028
Eric Delia <edelia@ncdot.gov>
2017-02-17 16:49:16 -0500

    more diamond modifications

--- app/modules/diamond.js
+++ app/modules/diamond.js
@@ -2,11 +2,24 @@ const diamond = (function() {
   const ball = document.querySelectorAll('.ball');
   
   for (let i = 0; i < ball.length; i++) {
-      const mc = new Hammer(ball[i]);
-      
-      mc.on('press', function(ev) {
-        console.log(ev.type);
-      });
+//      const mc = new Hammer(ball[i]);
+//      
+//      mc.on('press', function(ev) {
+//        console.log(ev);
+//      });
+    
+    ball[i].addEventListener('touchstart', function(e) {
+//      e.dataTransfer.effectAllowed = 'move';
+//      e.dataTransfer.setData('text', e.target);
+      console.log(e);
+    });
+    
+    ball[i].addEventListener('touchend', function(e) {
+      console.log(e);
+//      var data = e.dataTransfer.getData('text');
+//      console.log(data);
+//      e.target.appendChild(document.querySelector(data));
+    });
   }
   
 })();
