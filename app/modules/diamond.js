fad4960b55b1e6f2fe90968e9de48cb2a4fda120
escodel <escodel@gmail.com>
2017-02-17 23:47:00 -0500

    removed center diamond and adding first classes

--- app/modules/diamond.js
+++ app/modules/diamond.js
@@ -2,24 +2,26 @@ const diamond = (function() {
   const ball = document.querySelectorAll('.ball');
   
   for (let i = 0; i < ball.length; i++) {
-//      const mc = new Hammer(ball[i]);
-//      
-//      mc.on('press', function(ev) {
-//        console.log(ev);
-//      });
+      const mc = new Hammer(ball[i]);
+      
+      mc.on('press', function(ev) {        
+        const evTarget = ev.target;
+        evTarget.classList.toggle('ball-active');
+//        evTarget.nextSibling.classList.toggle('.ball-inactive');
+      });
     
-    ball[i].addEventListener('touchstart', function(e) {
+//    ball[i].addEventListener('touchstart', function(e) {
 //      e.dataTransfer.effectAllowed = 'move';
 //      e.dataTransfer.setData('text', e.target);
-      console.log(e);
-    });
-    
-    ball[i].addEventListener('touchend', function(e) {
-      console.log(e);
+//      console.log(e);
+//    });
+//    
+//    ball[i].addEventListener('touchend', function(e) {
+//      console.log(e);
 //      var data = e.dataTransfer.getData('text');
 //      console.log(data);
 //      e.target.appendChild(document.querySelector(data));
-    });
+//    });
   }
   
 })();
