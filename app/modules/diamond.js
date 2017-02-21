5b313705ddfb6bc13fc8068a5dec6cae053dfe39
escodel <escodel@gmail.com>
2017-02-20 22:03:58 -0500

    dead ball logic added

--- app/modules/diamond.js
+++ app/modules/diamond.js
@@ -2,9 +2,27 @@ const diamond = (function () {
   const ball = document.querySelectorAll('.ball');
 
   for (let i = 0; i < ball.length; i++) {
-//    const mc = new Hammer(ball[i]);
-//
-//    mc.on('press', function (ev) {
+    // Dead Balls
+    const mc = new Hammer(ball[i]);
+    
+    mc.on('press', function (ev){
+      var deadBallTarget = ev.target;
+      var ballArray = Array.from(ball);
+      var classListArray = Array.from(ev.target.classList);
+      var targetClass = classListArray[1];
+      deadBallTarget.classList.add('ball-dead');
+      
+      ballArray
+        .filter(function(b) {
+          return b.classList.contains(targetClass);
+      })
+        .forEach(function(b) {
+          b.classList.add('ball-dead');
+      });
+      return false;
+    });
+    
+    // Active / Inactive
     ball[i].addEventListener('click', function (ev) {
       const evTarget = ev.target;
       if (!evTarget.classList.contains('ball-space')) {
@@ -20,6 +38,9 @@ const diamond = (function () {
           return !b.classList.contains('ball-space');
       })
         .filter(function(b) {
+          return !b.classList.contains('ball-dead');
+      })
+        .filter(function(b) {
           return b.classList.contains(targetClass);
       })
         .forEach(function (b) {
@@ -31,12 +52,14 @@ const diamond = (function () {
           b.classList.remove('ball-inactive');
         }
 
-        if (!b.classList.contains('ball-active' || 'ball-inactive')) {
+        if (!b.classList.contains('ball-active' || 'ball-inactive' || 'ball-dead')) {
           b.classList.add('ball-inactive');
           b.classList.remove('ball-active');
         }
       })
     });
+    
+
 //    })
   }
 })();
