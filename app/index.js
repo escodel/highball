c0e99e3d1118ab8d14782dd742de0ca6f21d7766
Eric Delia <edelia@ncdot.gov>
2017-02-21 09:16:00 -0500

    adding boilerplate values

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
 const incrementer = require('./modules/incrementer');
 const diamond = require('./modules/diamond');
 
-function component () {
-  var element = document.createElement('div');
-
-  element.innerHTML = ['Hello','individual'].join(' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+//function component () {
+//  var element = document.createElement('div');
+//
+//  element.innerHTML = ['Hello','individual'].join(' ');
+//
+//  return element;
+//}
+//
+//document.body.appendChild(component());
