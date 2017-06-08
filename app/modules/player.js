4ad33de0c78705697e5be8a265ef60101e60e2c0
Eric Delia <edelia@ncdot.gov>
2017-06-08 16:59:19 -0400

    fixing player defect

--- app/modules/player.js
+++ app/modules/player.js
@@ -4,25 +4,25 @@ const player = (function() {
   var playerTwo = document.getElementById('player-2-Name');
   var nineBall = document.querySelectorAll('.ball-9');
   
-  function checkPlayer() {
-    var playerOneName = document.getElementById('player-1-Name');
-    var playerTwoName = document.getElementById('player-2-Name');    
-    
-    if ('art vandelay' === playerOneName.innerHTML.toLowerCase() || playerTwoName.innerHTML.toLowerCase()) {
-      nineBall.forEach(function(ball) {
-        ball.classList.add('av');
-      });
-    }
-    
-    if ('ian malcolm' === playerOneName.innerHTML.toLowerCase() || playerTwoName.innerHTML.toLowerCase()) {
-      nineBall.forEach(function(ball) {
-        ball.classList.add('im');
-      });
-    }    
-  }
-  
-  playerOne.addEventListener('blur', checkPlayer);
-  playerTwo.addEventListener('blur', checkPlayer);
+//  function checkPlayer() {
+//    var playerOneName = document.getElementById('player-1-Name');
+//    var playerTwoName = document.getElementById('player-2-Name');    
+//    
+//    if ('art vandelay' === playerOneName.innerHTML.toLowerCase() || playerTwoName.innerHTML.toLowerCase()) {
+//      nineBall.forEach(function(ball) {
+//        ball.classList.add('av');
+//      });
+//    }
+//    
+//    if ('ian malcolm' === playerOneName.innerHTML.toLowerCase() || playerTwoName.innerHTML.toLowerCase()) {
+//      nineBall.forEach(function(ball) {
+//        ball.classList.add('im');
+//      });
+//    }    
+//  }
+//  
+//  playerOne.addEventListener('blur', checkPlayer);
+//  playerTwo.addEventListener('blur', checkPlayer);
 })();
 
 module.exports = player;
